8c29bd3ed64aa5c262482226fd2d5c0567c6c2e0
Teby4 <felipeoliveramantero@gmail.com>
2024-01-12 09:09:04 -0800

    fix bad checker xd

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,6 +19,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (iterator = 0; iterator < size - 1; iterator++)
 	{
+		print_array(array, size);
 		for (index = 0; index < size - iterator - 1; index++)
 		{
 			if (array[index] > array[index + 1])
