6f74e5393e6ffea67b274ee18adfc0e9efded26e
Nitsu47 <agustraversa2004@gmail.com>
2024-01-19 00:10:05 +0000

    task 2 - selection sort and big O

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array of integers in ascending,
+ * order using the Selection sort algorithm.
+ * @array: pointer to the array of integers.
+ * @size: amount of numbers.
+ * Return: sorted array.
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		unsigned int min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (i != min)
+		{
+			print_array(array, size);
+			swap(&array[min], &array[i]);
+    	}
+	}
+}
+/**
+ * swap - swaps two integers
+ * Return: a and b swaped
+ * @a: first thing for swap
+ * @b: second swap parameter
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
